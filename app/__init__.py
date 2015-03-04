3064a690ac0111f23eecc18c0c0ea555b9d5bd6c
Sean Frischmann <shcsirf@gmail.com>
2015-03-04 23:21:58 +0000

    changed the look, working on uploading images

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,7 @@ DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
+UPLOADED_PHOTOS_DEST = 'app/static/images'
 
 #create app
 app = Flask(__name__)
@@ -57,10 +58,29 @@ def show_entries():
 		i += 1
 	return render_template('show_entries.html', entries=entries)
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+	if request.method == 'POST' and 'photo' in request.files:
+		filename = photos.save(request.files['photo'])
+		rec = Photo(filename=filename, user=g.user.id)
+		rec.store()
+		flash("Photo saved.")
+		return redirect(url_for('show', id=rec.id))
+	return render_template('upload.html')
+
+@app.route('/photo/<id>')
+def show(id):
+	photo = Photo.load(id)
+	if photo is None:
+		abort(404)
+	url = photos.url(photo.filename)
+	return render_template('show.html', url=url, photo=photo)
+
 @app.route('/add', methods=['POST'])
 def add_entry():
 	if not session.get('logged_in'):
 		abort(401)
+	print (request.form['photo'])
 	g.db.execute('insert into entries (title, ingredients, review) values (?,?,?)', 
 			[request.form['title'], request.form['ingredients'], request.form['review']])
 	g.db.commit()
